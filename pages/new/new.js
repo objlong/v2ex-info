1633a7339bf254394214cb6c7a53f6fa52f81e94
objlong007 <466258187@qq.com>
2017-02-14 17:18:48 +0800

    增加最新主题页面

---
+++ pages/new/new.js
@@ -0,0 +1,23 @@
+var app = getApp();
+Page({
+    data: {
+        articleList: []
+    },
+    onLoad: function () {
+        var _this = this;
+        wx.request({
+            url: 'https://www.v2ex.com/api/topics/latest.json',
+            data: {},
+            method: 'GET',
+            success: function (res) {
+                console.log(res)
+                _this.setData({
+                    articleList: res.data
+                });
+            },
+            fail: function () {
+                alert('网络连接失败');
+            }
+        })
+    }
+});
